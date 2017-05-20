49f6974c2a3189299bb0392fd0d26cc55e4dbdad
Nik Yerra <nik_yerra@Niks-MacBook-Pro.local>
2017-05-20 17:09:58 -0500

    Test POST/todos route

---
+++ server/server.js
@@ -0,0 +1,31 @@
+//Lib imports
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//local imports
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json())
+
+app.post('/todos',(req, res) =>{
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e) =>{
+    res.status(400).send(e);
+  })
+});
+
+
+app.listen(3000, () =>{
+  console.log("Started on port 3000");
+});
+
+module.exports = {app};
